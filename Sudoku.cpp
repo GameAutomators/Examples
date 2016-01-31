0b63cf3eb69a388e7aedf90aa3cdc635efa1b93d
Sreetam <sreetamdas@gmail.com[filter lfs]>
2016-01-31 06:30:37 +0530

    Final Commit
    Hopefully

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,136 +1,86 @@
-#include <iostream>
 #include <stdio.h>
-
+#include <string.h>
 #define UNASSIGNED 0
 #define N 9
 
-using namespace std;
-int Solution(int mtr[N][N]);
-int Find(int mtr[N][N], int &row, int &col);
-int RowCheck(int mtr[N][N], int row, int num);
-int ColCheck(int mtr[N][N], int col, int num);
-int BoxCheck(int mtr[N][N], int boxStartRow, int boxStartCol, int num);
-int Check(int mtr[N][N], int row, int col, int num);
-void Display(int mtr[N][N]);
-
-
-int Solution(int mtr[N][N])
+bool FindUnassignedLocation(int grid[N][N], int &row, int &col);
+bool isSafe(int grid[N][N], int row, int col, int num);
+bool SolveSudoku(int grid[N][N])
 {
     int row, col;
- 
-    if (!Find(mtr, row, col))
-    {
-       return 1;
-    }
- 
+    if (!FindUnassignedLocation(grid, row, col))
+       return true;
     for (int num = 1; num <= 9; num++)
     {
-        if (Check(mtr, row, col, num))
+        if (isSafe(grid, row, col, num))
         {
-
-            mtr[row][col] = num;
-
-            if (Solution(mtr))
-            {
-                return 1;
-            }
- 
-            mtr[row][col] = UNASSIGNED;
+            grid[row][col] = num;
+            if (SolveSudoku(grid))
+                return true;
+            grid[row][col] = UNASSIGNED;
         }
     }
-    return 0;
+    return false;
 }
-
-int Find(int mtr[N][N], int &row, int &col)
+bool FindUnassignedLocation(int grid[N][N], int &row, int &col)
 {
     for (row = 0; row < N; row++)
-    {
         for (col = 0; col < N; col++)
-        {
-            if (mtr[row][col] == UNASSIGNED)
-            {
-                return 1;
-            }
-        }
-    }
-    return 0;
+            if (grid[row][col] == UNASSIGNED)
+                return true;
+    return false;
 }
-
-int RowCheck(int mtr[N][N], int row, int num)
+bool UsedInRow(int grid[N][N], int row, int num)
 {
     for (int col = 0; col < N; col++)
-    {
-        if (mtr[row][col] == num)
-        {
-            return 1;
-        }
-    }
-    return 0;
+        if (grid[row][col] == num)
+            return true;
+    return false;
 }
-int ColCheck(int mtr[N][N], int col, int num)
+bool UsedInCol(int grid[N][N], int col, int num)
 {
     for (int row = 0; row < N; row++)
-    {
-        if (mtr[row][col] == num)
-        {
-            return 1;
-        }
-    }
-    return 0;
+        if (grid[row][col] == num)
+            return true;
+    return false;
 }
-
-int BoxCheck(int mtr[N][N], int boxStartRow, int boxStartCol, int num)
+bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num)
 {
     for (int row = 0; row < 3; row++)
-    {
         for (int col = 0; col < 3; col++)
-        {
-            if (mtr[row+boxStartRow][col+boxStartCol] == num)
-            {
-                return 1;
-            }
-        }
-    }
-    return 0;
+            if (grid[row+boxStartRow][col+boxStartCol] == num)
+                return true;
+    return false;
 }
-
-int Check(int mtr[N][N], int row, int col, int num)
+bool isSafe(int grid[N][N], int row, int col, int num)
 {
-    return !RowCheck(mtr, row, num) && !ColCheck(mtr, col, num) && !BoxCheck(mtr, row - row%3 , col - col%3, num);
+    return !UsedInRow(grid, row, num) &&
+           !UsedInCol(grid, col, num) &&
+           !UsedInBox(grid, row - row%3 , col - col%3, num);
 }
-
-void Display(int mtr[N][N])
+void printGrid(int grid[N][N])
 {
     for (int row = 0; row < N; row++)
     {
        for (int col = 0; col < N; col++)
-    	{
-    		cout<<mtr[row][col];
-		}
-		cout<<"\n";
+             printf("%d", grid[row][col]);
     }
 }
-
-int main()
+int main(int argc, char * argv[])
 {
-    int mtr[N][N];
-    cout<<"Enter the matrix:\n";
-	for(int i=0;i<N;i++)
-	{
-		for(int j=0;j<N;j++)
-		{
-			cin>>mtr[i][j];
-		}
-	}
-	
-	if (Solution(mtr) == 1)
-    {
-		Display(mtr);
-	}
+	int i,j; int grid[9][9];
+    if(argc ==1 || strlen(argv[1]) != 81) {
+        printf("-1" );
+        return 1;
+    }
+    for(i=0; i<81; i++){
+    	grid[i/9][i%9] = argv[1][i]-'0';
+        //grid[i/9][i%9] = 0;
+    }
+    if ( SolveSudoku(grid) == true )
+          printGrid(grid);
     else
-	{
-		cout<<"No solution exists";
-	}
- 
+         printf("-1");
     return 0;
 }
+//000000000000000000000000000000000000000000000000000000000000000000000000
